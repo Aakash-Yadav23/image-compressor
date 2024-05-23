b2f102e5fb3a73403efdd26c23fda384935243d8
developer+2-at-118321773388 <aakashdev24@gmail.com>
2024-05-23 08:25:22 +0530

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center dark justify-center p-24">
+    <main className="flex h-screen flex-col items-center dark justify-center p-6">
       <div className="py-5 pb-10 font-bold">
         <h1 className="text-2xl">
           Image Compressor
