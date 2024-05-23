7be315a47abf4d350b63997c269aa42ac835a5b2
developer+2-at-118321773388 <aakashdev24@gmail.com>
2024-05-23 07:55:22 +0530

    complete

--- src/util/compressImage.ts
+++ src/util/compressImage.ts
@@ -2,20 +2,19 @@
 export const compressImage = (base64Str: string, maxSizeKB: number, callback: (compressedBase64: string) => void) => {
     const img = new Image();
     img.src = base64Str;
-  
+
     img.onload = () => {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      if (!ctx) return;
-  
-      const scaleFactor = Math.sqrt(maxSizeKB / (base64Str.length / 1024));
-      canvas.width = img.width * scaleFactor;
-      canvas.height = img.height * scaleFactor;
-  
-      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      const compressedBase64 = canvas.toDataURL('image/jpeg', 0.9); // Adjust quality as needed
-  
-      callback(compressedBase64);
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+
+        const scaleFactor = Math.sqrt(maxSizeKB / (base64Str.length / 1024));
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        const compressedBase64 = canvas.toDataURL('image/jpeg', 0.9); // Adjust quality as needed
+
+        callback(compressedBase64);
     };
-  };
-  
+};
